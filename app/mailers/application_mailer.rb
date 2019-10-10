49ab84e00e2b01f7c8180e0a8a5be6137873a312
occmium <occkart@yahoo.com>
2019-10-10 17:03:17 +0600

    Lesson_58 ~Edit/layout/after$ rails g devise:i18n:views

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'omskattitude@narod.ru '
+  default from: 'omskattitude@narod.ru'
   # default from: 'ramantimov@gmail.com'
   layout 'mailer'
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'from@gmail.com'
+  config.mailer_sender = 'omskattitude@narod.ru'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -166,7 +166,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 3..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
