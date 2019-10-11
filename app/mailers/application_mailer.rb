0c3fca8340ff33b53ba3c32fa821742c04b16b45
occmium <occkart@yahoo.com>
2019-10-11 15:11:41 +0600

    Task-58-3

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'ramantimov@gmail.com'
-  default from: 'no_reply@suppo.rt'
+  # default from: 'no_reply@suppo.rt'
+  default from: 'omskattitude@narod.ru'
   layout 'mailer'
 end
