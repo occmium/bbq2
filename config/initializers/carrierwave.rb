741850f12728a56486c9eec58898b677c99d7eb4
occmium <occkart@yahoo.com>
2019-10-24 13:05:15 +0600

    Task_66-1 before deploy DigitalOcean

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,12 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider:              'AWS',
-      region:                'eu-north-1',
-      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      provider:              Rails.application.credentials.aws[:provider],
+      region:                Rails.application.credentials.aws[:region],
+      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
     }
 
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_directory  = Rails.application.credentials.aws[:bucket_name]
   end
 end
